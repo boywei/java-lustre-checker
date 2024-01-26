f55d2e10802fadf3952a6689c21938c940f23780
Jiang <2575890444@qq.com>
2024-01-26 16:00:11 +0800

    。。

---
+++ src/test/java/com/ecnu/synlong/SynlongApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ecnu.synlong;
+
+import com.ecnu.synlong.api.Kind2Api4Synlong;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynlongApplicationTests {
+
+
+	@Test
+	void testApi4Synlong() {
+
+	}
+
+}
